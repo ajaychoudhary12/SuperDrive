903036a3ba7e39cc8d0ccadb76c53153b8354fbf
Ajay <ajaychoudhary19990@gmail.com>
2024-04-20 11:45:28 +0530

    WIP: Auth Module

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/User.java
@@ -0,0 +1,68 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+public class User {
+    private Integer userId;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String salt;
+    private String password;
+
+
+    public User(Integer userId, String username, String firstName, String lastName, String password, String salt) {
+        this.userId = userId;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.salt = salt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
